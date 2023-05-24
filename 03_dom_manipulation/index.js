6b6182153df79076547c460d8027f9956c42bf47
Adam La Rosa <adam.larosa@flatironschool.com>
2023-05-24 05:19:49 -0700

    added deliverable

--- 03_dom_manipulation/index.js
+++ 03_dom_manipulation/index.js
@@ -1,19 +1,29 @@
 
 
-// 1. find a DOM element => .querySelector()
+/* 1. find a DOM element => .querySelector() */
 
-// 2. find a collection of DOM elements => .querySelectorAll()
+/* 2. find a collection of DOM elements => .querySelectorAll() */
 
-// 3. find a DOM element by its ID => .getElementById()
+/* 3. find a DOM element by its ID => .getElementById() */
 
-// 4. find a collection of elements by class name => .getElementsByClassName()
+/* 4. find a collection of elements by tag name .getElementsByTagName() */
 
-// 5. create a new element => .createElement()
+/* 5. find a collection of elements by class => .getElementsByClassName() */
 
-// 6. append an element to the DOM => .appendChild()
+/* 6. create a new element => .createElement() */
 
-// 7. remove an element from the DOM => .remove()
+/* 7. append an element to the DOM => .append() & .appendChild() */
 
-// 8. change the text inside an element => .textContent() & .innerText()
+/* 8. remove an element from the DOM => .remove() */
 
-// 9. set the HTML inside an element => .innerHTML()
+/* 9. change the text inside an element => .textContent & .innerText */
+
+/* 10. set the HTML inside an element => .innerHTML */
+
+/*
+    BONUS!
+
+  11. see the parent of an element => .parentElement()
+
+  12. see the children of an element => .children
+*/
