b69eccd778e9ddcd03913534af28793212747bfa
Adam La Rosa <adam.larosa@flatironschool.com>
2023-05-24 08:31:20 -0700

    solution update

---
+++ 03_dom_manipulation/index.js
@@ -0,0 +1,29 @@
+
+
+/* 1. find a DOM element => .querySelector() */
+
+/* 2. find a collection of DOM elements => .querySelectorAll() */
+
+/* 3. find a DOM element by its ID => .getElementById() */
+
+/* 4. find a collection of elements by tag name .getElementsByTagName() */
+
+/* 5. find a collection of elements by class => .getElementsByClassName() */
+
+/* 6. create a new element => .createElement() */
+
+/* 7. append an element to the DOM => .append() & .appendChild() */
+
+/* 8. remove an element from the DOM => .remove() */
+
+/* 9. change the text inside an element => .textContent & .innerText */
+
+/* 10. set the HTML inside an element => .innerHTML */
+
+/*
+    BONUS!
+
+  11. see the parent of an element => .parentElement()
+
+  12. see the children of an element => .children
+*/
