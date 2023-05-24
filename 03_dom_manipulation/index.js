28b1ff413c34780905d6c0f62bb1b0f14950aa3d
Adam La Rosa <adam.larosa@flatironschool.com>
2023-05-24 04:54:43 -0700

    starter

---
+++ 03_dom_manipulation/index.js
@@ -0,0 +1,19 @@
+
+
+// 1. find a DOM element => .querySelector()
+
+// 2. find a collection of DOM elements => .querySelectorAll()
+
+// 3. find a DOM element by its ID => .getElementById()
+
+// 4. find a collection of elements by class name => .getElementsByClassName()
+
+// 5. create a new element => .createElement()
+
+// 6. append an element to the DOM => .appendChild()
+
+// 7. remove an element from the DOM => .remove()
+
+// 8. change the text inside an element => .textContent() & .innerText()
+
+// 9. set the HTML inside an element => .innerHTML()
