8adef1c92880044ff21f25756c1b0b6d923359e7
Emiley Palmquist <54187547+emileypalmquist@users.noreply.github.com>
2023-06-16 07:23:05 -0700

    starter code demo useEffect

---
+++ 15_side_effects_and_data_fetching/use_effect_demo/src/components/App.js
@@ -0,0 +1,14 @@
+import Child from "./Child";
+
+function App() {
+  console.log("App Render");
+
+  return (
+    <div className="App">
+      App
+      <Child />
+    </div>
+  );
+}
+
+export default App;
